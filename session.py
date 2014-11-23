cb46d4d20ce8856bf6bd55ce9960267c8c89ccc3
younger.shen <younger.x.shen@gmail.com>
2014-11-24 00:12:14 +0800

    change dir structure

--- session.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-#-*- coding: utf-8 -*-
-
-# author : younger shen
-# email  : younger.x.shen@gmail.com
-
-
---
+++ xsession/session.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+
+# author : younger shen
+# email  : younger.x.shen@gmail.com
+
+"""
+    xsession.session
+    ~~~~~~~~~~~~~~~~
+
+    session entity to manage
+
+    :copyright: (c) 2014 by younger shen
+    :license: GPL, see LICENSE for more details
+"""
+
+
+
